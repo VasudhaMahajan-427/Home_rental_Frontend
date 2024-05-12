f82c56c7e251435d9b2a0e7dcd5181d5f3b896ef
VasudhaMahajan-427 <152297147+VasudhaMahajan-427@users.noreply.github.com>
2024-05-12 13:53:12 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/*import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store'; // Import your Redux store configuration
+
+import App from './App'; // Your main application component
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
